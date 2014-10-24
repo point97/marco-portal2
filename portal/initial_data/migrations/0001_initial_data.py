fa0b0b6d9bb037dd20688d39ba8eb249f4aa20d1
Eric Drechsel <eric@pointnineseven.com>
2014-10-24 15:08:18 -0700

    add calendar to initial data migration

---
+++ portal/initial_data/migrations/0001_initial_data.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from wagtail.wagtailcore.models import Page
+
+def create_initial_data(apps, schema_editor):
+    home_page = Page.objects.get(url_path="/home/")
+    ContentType = apps.get_model('contenttypes.ContentType')
+
+    top_level_pages = [
+        {'app': 'ocean_stories', 'model_name': 'OceanStories', 'title': 'Ocean Stories', 'content_type': 'oceanstories'},
+        {'app': 'calendar', 'model_name': 'Calendar', 'title': 'Calendar', 'content_type': 'calendar'},
+    ]
+
+    for p in top_level_pages:
+        model = apps.get_model('%s.%s' % (p['app'], p['model_name']))
+        content_type, created = ContentType.objects.get_or_create(
+            model=p['content_type'], app_label=p['app'], defaults={'name': p['title']})
+
+        home_page.add_child(instance=model(
+            title=p['title'],
+            live=False,
+            content_type=content_type,
+        ))
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('ocean_stories', '0001_initial'),
+        ('home', '0002_create_homepage'),
+        ('calendar', '__latest__'),
+
+    ]
+
+    operations = [
+        migrations.RunPython(create_initial_data),
+    ]
