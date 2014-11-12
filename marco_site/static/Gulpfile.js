d58c31f548f9dd40725c91f88aad2aa66c4fa46f
Eric Drechsel <eric@pointnineseven.com>
2014-11-12 15:31:18 -0800

    glyphicons

--- marco_site/static/Gulpfile.js
+++ marco_site/static/Gulpfile.js
@@ -17,6 +17,17 @@ gulp.task('bower', function() {
         .pipe(gulp.dest(config.bowerDir))
 });
 
+gulp.task('icons', function() {
+    return gulp.src(config.bowerDir + '/bootstrap-sass-official/assets/fonts/bootstrap/*')
+        .pipe(gulp.dest('./fonts/bootstrap'));
+});
+
+gulp.task('copyjs', function() {
+    return gulp.src(config.bowerDir + '/bootstrap-sass-official/assets/javascripts/bootstrap.js')
+        .pipe(gulp.dest('./js'));
+});
+
+
 gulp.task('css', function() {
     return gulp.src(config.sassPath + '/*.scss')
         .pipe(sass({
@@ -37,4 +48,4 @@ gulp.task('watch', function() {
     gulp.watch(config.sassPath + '/**/*.scss', ['css']);
 });
 
-gulp.task('default', ['bower', 'css']);
+gulp.task('default', ['bower', 'icons', 'copyjs', 'css']);
