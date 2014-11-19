b613eb2dfb7c56606b31c8e1e7370cef90f4eaf9
Eric Drechsel <eric@pointnineseven.com>
2014-11-19 15:27:11 -0800

    implement scaffold index view for ocean stories, using new index_listing tag

---
+++ portal/base/templatetags/portal.py
@@ -0,0 +1,17 @@
+from django import template
+
+register = template.Library()
+
+# Retrieves all live pages which are children of the calling page
+#for standard index listing
+@register.inclusion_tag(
+    'portal/tags/index_listing.html',
+    takes_context=True
+)
+def index_listing(context, calling_page):
+    pages = calling_page.get_children().filter(live=True)
+    return {
+        'pages': pages,
+        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
