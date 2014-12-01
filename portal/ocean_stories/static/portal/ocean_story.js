f131f00b0071425b9423fcf2e3a3f086efa23aec
Eric Drechsel <eric@pointnineseven.com>
2014-12-01 16:26:14 -0700

    first pass at ocean story map view
    loads and switches section state:
    
     - map view
     - base layer

--- portal/ocean_stories/static/portal/ocean_story.js
+++ portal/ocean_stories/static/portal/ocean_story.js
@@ -1,28 +1,22 @@
 var OceanStory = (function() {
 
-  function ifChangedSetter(set, remove) {
+  function baseLayerSetter(map, baseLayers) {
     var current;
     return function(value) {
+      // noop
       if (value == current) return;
-      // return if set failed
-      if (!set(value)) return;
-      if (current) remove(current);
+
+      // return early if layer is unknown
+      if (!baseLayers.hasOwnProperty(value)) return;
+
+      // add layer
+      map.addLayer(baseLayers[value]);
+
+      // remove old layer if there is one
+      if (current) map.removeLayer(baseLayers[value]);
+
       current = value;
     }
-  }
-
-  /* if obj.k exists, call f(obj[k]) */
-  function ifExistsCaller(obj, f) {
-    return function(k) {
-      return obj.hasOwnProperty(k) ? f(obj[k]) : false;
-    }
-  }
-
-  function baseLayerSetter(map, baseLayers) {
-    return ifChangedSetter(
-      ifExistsCaller(baseLayers, function(l){return map.addLayer(l)}),
-      ifExistsCaller(baseLayers, function(l){return map.removeLayer(l)})
-    )
   }
 
   function create(selector, story, layerCatalog) {
@@ -34,10 +28,11 @@ var OceanStory = (function() {
       doubleClickZoom: false,
       boxZoom: false,
       tap: false,
+      keyboard: false,
+      attributionControl: false,
     });
     var activeSection;
 
-    var dataLayers = {};
     var setBaseLayer = baseLayerSetter(map, {
       "Esri Oceans": L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
         attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
@@ -49,7 +44,7 @@ var OceanStory = (function() {
       "Google Street": new L.Google('STREET'),
       "Google Terrain": new L.Google('TERRAIN'),
       "Google Satellite": new L.Google('SATELLITE'),
-    })
+    });
 
     function goToSection(section) {
       s = story.sections[section];
