c19fe55a208f0e774d957a8839d1184192a837fe
Eric Drechsel <eric@pointnineseven.com>
2014-12-11 15:16:07 -0800

    move dev db settings to dev module

--- marco_config/settings/dev.py
+++ marco_config/settings/dev.py
@@ -4,3 +4,16 @@ TEMPLATE_DEBUG = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ALLOWED_HOSTS = []
+
+# PostgreSQL (Recommended, but requires the psycopg2 library and Postgresql development headers)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'marco_portal',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',  # Set to empty string for localhost.
+        'PORT': '',  # Set to empty string for default.
+        'CONN_MAX_AGE': 600,  # number of seconds database connections should persist for
+    }
+}
