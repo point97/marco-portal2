b8e8053167d901fd1e4e2f5d1daf42159200eca8
Eric Drechsel <eric@pointnineseven.com>
2014-12-11 14:59:43 -0800

    fix locale code

--- marco_config/settings/__init__.py
+++ marco_config/settings/__init__.py
@@ -108,7 +108,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-gb'
+LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
