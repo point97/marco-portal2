fbb5337d1425a77a0850659b597ab87cb111cd8e
Eric Drechsel <eric@pointnineseven.com>
2014-12-11 14:51:25 -0800

    reorganize settings so it's not necessary to set DJANGO_SETTINGS_MODULE in production

--- marco_config/settings/dev.py
+++ marco_config/settings/dev.py
@@ -1,13 +1,6 @@
-from .base import *
-
-
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 TEMPLATE_DEBUG = True
-
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-
-try:
-    from .local import *
-except ImportError:
-    pass
+ALLOWED_HOSTS = []
