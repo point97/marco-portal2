7af9154c027de91961946a2f7857a5432c1b7f57
Eric Drechsel <eric@pointnineseven.com>
2014-12-18 11:52:38 -0800

    add polyfill for pointer-events css property on ie<11

---
+++ portal/ocean_stories/static/portal/ocean_story/polyfills.js
@@ -0,0 +1,5 @@
+var pointerEventsPolyfill = require('exports?PointerEventsPolyfill!bower_components/pointer_events_polyfill/pointer_events_polyfill.js');
+
+$(document).ready(function(){
+  pointerEventsPolyfill.initialize({});
+});
