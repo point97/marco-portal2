314c284856970e8151569cee6f36dcd106bcaabd
Eric Drechsel <eric@pointnineseven.com>
2014-12-18 13:17:31 -0800

    limit pointer events polyfill to .overlay

--- portal/ocean_stories/static/portal/ocean_story/polyfills.js
+++ portal/ocean_stories/static/portal/ocean_story/polyfills.js
@@ -1,5 +1,7 @@
 var pointerEventsPolyfill = require('exports?PointerEventsPolyfill!bower_components/pointer_events_polyfill/pointer_events_polyfill.js');
 
 $(document).ready(function(){
-  pointerEventsPolyfill.initialize({});
+  pointerEventsPolyfill.initialize({
+    selector: '.overlay',
+  });
 });
