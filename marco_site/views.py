00846acbbde670210fe1e9d0d94ac9d897fb6fcf
Eric Drechsel <eric@pointnineseven.com>
2014-12-23 11:19:31 -0800

    POR-141 hide styleguide from non-privileged users

--- marco_site/views.py
+++ marco_site/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.contrib.auth.decorators import permission_required
 
+@permission_required('wagtailadmin.access_admin')
 def styleguide(request, template='marco_site/styleguide.html'):
     return render_to_response(template, RequestContext(request, {}))
