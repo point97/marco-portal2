8a49f2f366ae82f61cba3841f23558ee17422f97
Eric Drechsel <eric@pdxhub.org>
2015-01-02 14:06:20 -0800

    reorg index_listing templates

--- portal/base/templatetags/portal.py
+++ portal/base/templatetags/portal.py
@@ -7,7 +7,7 @@ register = template.Library()
 # Retrieves all live pages which are children of the calling page
 #for standard index listing
 @register.inclusion_tag(
-    'portal/tags/index_listing.html',
+    'portal/components/index_listing.html',
     takes_context=True
 )
 def index_listing(context, parent_page, item_template):
