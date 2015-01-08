935bdd6e6af028cd0809a8c716304028fb4ec287
Eric Drechsel <eric@pdxhub.org>
2015-01-08 15:37:12 -0800

    run vagrant provision unprivileged (fixes issue with su and ssh forwarding)

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -2,8 +2,8 @@
 
 PROJECT_NAME=$1
 
-PROJECT_DIR=/home/vagrant/$PROJECT_NAME
-VIRTUALENV_DIR=/home/vagrant/.virtualenvs/$PROJECT_NAME
+PROJECT_DIR=~/$PROJECT_NAME
+VIRTUALENV_DIR=~/.virtualenvs/$PROJECT_NAME
 
 APP_DB_NAME=$PROJECT_NAME
 
@@ -14,26 +14,26 @@ PIP=$VIRTUALENV_DIR/bin/pip
 # apt-get install -y python-opencv python-numpy
 
 # Virtualenv setup for project
-su - vagrant -c "/usr/local/bin/virtualenv --system-site-packages $VIRTUALENV_DIR && \
+/usr/local/bin/virtualenv --system-site-packages $VIRTUALENV_DIR && \
     source $VIRTUALENV_DIR/bin/activate && \
     echo $PROJECT_DIR > $VIRTUALENV_DIR/.project && \
     ssh-keyscan -H bitbucket.org >> ~/.ssh/known_hosts && \
     cd $PROJECT_DIR && \
-    $PIP install --src ./deps -r requirements.txt"
+    $PIP install --src ./deps -r requirements.txt
 
 echo "workon $PROJECT_NAME" >> /home/vagrant/.bashrc
 
-$PROJECT_NAME/scripts/reset_db $PROJECT_NAME
+# $PROJECT_NAME/scripts/reset_db $PROJECT_NAME
 
 # Set execute permissions on manage.py as they get lost if we build from a zip file
 chmod a+x $PROJECT_DIR/manage.py
 
 # Run syncdb/migrate/update_index
-su - vagrant -c "$PYTHON $PROJECT_DIR/manage.py migrate --noinput && \
-                 $PYTHON $PROJECT_DIR/manage.py update_index"
+$PYTHON $PROJECT_DIR/manage.py migrate --noinput && \
+$PYTHON $PROJECT_DIR/manage.py update_index
 
 # Add a couple of aliases to manage.py into .bashrc
-cat << EOF >> /home/vagrant/.bashrc
+cat << EOF >> ~/.bashrc
 alias dj="$PYTHON $PROJECT_DIR/manage.py"
 alias djrun="dj runserver 0.0.0.0:8000"
 EOF
