115d3a203d231463e0eac987d434e71b7f1c12e3
Eric Drechsel <eric@squaucer.local>
2015-01-21 15:57:43 -0800

    POR-212 phase 1: add custom image and rendition models

--- portal/base/models.py
+++ portal/base/models.py
@@ -1,11 +1,41 @@
 from django.db import models
+from django.dispatch.dispatcher import receiver
+from django.db.models.signals import pre_delete
 
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailsearch import index
 from wagtail.wagtailadmin.edit_handlers import FieldPanel,MultiFieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
-from wagtail.wagtailimages.models import Image
+from wagtail.wagtailimages.models import AbstractImage, AbstractRendition
+
+
+# Portal defines its own custom image class to replace wagtailimages.Image,
+# providing various additional data fields
+class PortalImage(AbstractImage):
+    pass
+
+
+# Receive the pre_delete signal and delete the file associated with the model instance.
+@receiver(pre_delete, sender=PortalImage)
+def image_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.file.delete(False)
+
+class PortalRendition(AbstractRendition):
+    image = models.ForeignKey('PortalImage', related_name='renditions')
+
+    class Meta:
+        unique_together = (
+            ('image', 'filter', 'focal_point_key'),
+        )
+
+# Receive the pre_delete signal and delete the file associated with the model instance.
+@receiver(pre_delete, sender=PortalRendition)
+def rendition_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.file.delete(False)
+
 
 class PageBase(Page):
     is_abstract = True
@@ -36,7 +66,7 @@ class DetailPageBase(PageBase):
         abstract = True
 
     feature_image = models.ForeignKey(
-        'wagtailimages.Image',
+        'PortalImage',
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
