3b8f3d97d4f9e1221652432e0c57ff9f555f1649
Seth Hill <seth@pointnineseven.com>
2015-03-05 11:37:59 -0800

    MP-839: Add STATIC_ROOT setting

--- marco/marco_config/settings.py
+++ marco/marco_config/settings.py
@@ -146,7 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = cfg.get('APP', 'STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
