e2738b59906d62f5281d7c3f4e6b2620d1f1bf61
Seth Hill <seth@pointnineseven.com>
2015-03-11 16:00:42 -0700

    Add MEDIA_ROOT config options

--- marco/marco/settings.py
+++ marco/marco/settings.py
@@ -158,7 +158,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_ROOT = cfg.get('APP', 'STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
-STATIC_URL = '/static/'
+STATIC_URL = cfg.get('APP', 'STATIC_URL', '/static/')
 
 STATICFILES_DIRS = (
     STYLES_DIR,
@@ -170,8 +170,8 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = cfg.get('APP', 'MEDIA_ROOT', os.path.join(BASE_DIR, 'media'))
+MEDIA_URL = cfg.get('APP', 'MEDIA_URL', '/media/')
 
 # Django compressor settings
 COMPRESS_PRECOMPILERS = (
