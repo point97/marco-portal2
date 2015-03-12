4c875e312b35e19392c395da1bfe75bf61137b0b
Seth Hill <seth@pointnineseven.com>
2015-03-12 15:45:13 -0700

    Add wsgi file for 468

---
+++ marco/marco/wsgi_web468.py
@@ -0,0 +1,11 @@
+"""WSGI File for Web468
+"""
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "marco.settings")
+activate_this = '/home/point97/env/marco_portal2/bin/activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
