87616da225211318b1f3e35e982e59edd8c58f14
Drew <drew@pointnineseven.com>
2015-05-28 13:47:13 -0700

    constrain search to 2 chars and remove unnecessary models

--- marco/portal/base/views.py
+++ marco/portal/base/views.py
@@ -5,8 +5,14 @@ from django.template import RequestContext
 
 from wagtail.wagtailcore import models
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailimages.models import Image
 from wagtail.wagtailsearch.index import get_indexed_models
 from wagtail.wagtailsearch.backends import get_search_backend
+
+from portal.base.models import PortalImage
+from portal.ocean_stories.models import OceanStory, OceanStories
+from portal.calendar.models import Event
+from portal.news.models import News, Story
 
 from data_manager.models import Layer, Theme
 
@@ -15,25 +21,27 @@ register = template.Library()
 def search(request, template=settings.WAGTAILSEARCH_RESULTS_TEMPLATE):
     query_string = request.GET.get('q', '')
 
-    if query_string != '':
-        ocean_story_results = []
-        calendar_news_results = []
-        data_needs_results = []
-        resources_results = []
-        theme_results = []
-        layer_results = []
+    ocean_story_results = []
+    calendar_news_results = []
+    data_needs_results = []
+    resources_results = []
+    theme_results = []
+    layer_results = []
+
+    if len(query_string) >= 2:
         s = get_search_backend()
         models = get_indexed_models()
-        # remove redundant model
-        models.remove(Page)
+        # remove unnecessary models
+        for i in [Page, Image, PortalImage]:
+            models.remove(i)
 
         # search wagtail pages
         for model in models:
             results = s.search(query_string, model)
             for item in results:
-                if '/ocean-stories/' in item.url:
+                if isinstance(item, (OceanStory, OceanStories)):
                     ocean_story_results.append(item)
-                elif '/calendar/' in item.url or '/news/' in item.url:
+                elif isinstance(item, (Event, News, Story)):
                     calendar_news_results.append(item)
                 elif '/data-needs-and-priorities/' in item.url:
                     data_needs_results.append(item)
