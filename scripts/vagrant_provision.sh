8860ca4e34b108d6cbc7406aff7f02028e4ad64a
Drew <drew@pointnineseven.com>
2016-02-25 12:56:44 -0800

    vagrant now uses local marco apps as dependencies

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 PROJECT_NAME=$1
 APP_NAME=$2
 
@@ -15,20 +14,39 @@ PIP=$VIRTUALENV_DIR/bin/pip
 # apt-get install -y python-opencv python-numpy
 
 # Virtualenv setup for project
+echo "setting up virtualenvs"
 /usr/local/bin/virtualenv --system-site-packages $VIRTUALENV_DIR && \
     source $VIRTUALENV_DIR/bin/activate && \
     echo $PROJECT_DIR > $VIRTUALENV_DIR/.project && \
     cd $PROJECT_DIR && \
-    $PIP install --src ./apps -r requirements.txt
+    echo "installing project dependencies"
+    $PIP install --src ./deps -r dev_requirements.txt && \
+    $PIP install -e $PROJECT_DIR/apps/madrona-analysistools && \
+    $PIP install -e $PROJECT_DIR/apps/madrona-features && \
+    $PIP install -e $PROJECT_DIR/apps/madrona-forms && \
+    $PIP install -e $PROJECT_DIR/apps/madrona-scenarios && \
+    $PIP install -e $PROJECT_DIR/apps/madrona-manipulators && \
+    $PIP install -e $PROJECT_DIR/apps/mp-clipping && \
+    $PIP install -e $PROJECT_DIR/apps/mp-drawing && \
+    $PIP install -e $PROJECT_DIR/apps/mp-explore && \
+    $PIP install -e $PROJECT_DIR/apps/mp-accounts && \
+    $PIP install -e $PROJECT_DIR/apps/mp-visualize && \
+    $PIP install -e $PROJECT_DIR/apps/mp-data-manager && \
+    $PIP install -e $PROJECT_DIR/apps/mp-proxy && \
+    $PIP install -e $PROJECT_DIR/apps/marco-map_groups && \
+    $PIP install -e $PROJECT_DIR/apps/p97-nursery && \
+    $PIP install -e $PROJECT_DIR/apps/p97settings
 
 echo "workon $PROJECT_NAME" >> /home/vagrant/.bashrc
 
+echo "resetting DB"
 $PROJECT_DIR/scripts/reset_db $3
 
 # Set execute permissions on manage.py as they get lost if we build from a zip file
 chmod a+x $PROJECT_DIR/$APP_NAME/manage.py
 
 # Run syncdb/migrate/update_index
+echo "database syncing and migrations"
 $PYTHON $PROJECT_DIR/$APP_NAME/manage.py makemigrations && \
 $PYTHON $PROJECT_DIR/$APP_NAME/manage.py migrate --noinput && \
 $PYTHON $PROJECT_DIR/$APP_NAME/manage.py update_index
